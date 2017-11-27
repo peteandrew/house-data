49dae561d6545b968f21c9f1bec4b25a6eeff885
Peter Andrew <peteandrew101@gmail.com>
2017-11-27 13:54:57 +0000

    Initial commit

---
+++ gateway/gpio_temp.py
@@ -0,0 +1,31 @@
+# Adapted from https://learn.adafruit.com/adafruits-raspberry-pi-lesson-11-ds18b20-temperature-sensing/software
+
+import os
+import glob
+import time
+ 
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+ 
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
+ 
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp():
+    lines = read_temp_raw()
+    if lines[0].strip()[-3:] != 'YES':
+        return None
+
+    equals_pos = lines[1].find('t=')
+    if equals_pos == -1:
+        return None
+
+    temp_string = lines[1][equals_pos+2:]
+    temp_c = float(temp_string) / 1000.0
+    return temp_c
