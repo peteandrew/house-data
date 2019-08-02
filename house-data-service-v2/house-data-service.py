9d1a20655d00704eead154af2dfc667db7fa67cc
Pete Andrew <peteandrew101@gmail.com>
2019-08-02 23:23:02 +0100

    Changed data endpoint to nodes. Added query param to specify sensor type. Fetch all sendor data for node if type not specified

--- house-data-service-v2/house-data-service.py
+++ house-data-service-v2/house-data-service.py
@@ -1,38 +1,55 @@
-from flask import Flask, g, jsonify, abort
+from flask import Flask, g, jsonify, abort, request
 import boto3
 
 app = Flask(__name__)
 
 client = boto3.client('dynamodb')
 
-@app.route("/last_temps/<int:node>")
-def last_temps(node):
-    node_and_type = '{}_T'.format(str(node))
-    response = client.query(
-        TableName='HouseData',
-        KeyConditionExpression='NodeAndType = :node_and_type',
-        ExpressionAttributeValues={
-            ':node_and_type': {
-                'S': str(node_and_type)
-            }
-        },
-        ScanIndexForward=False,
-        Limit=1
-    )
+@app.route("/nodes/<int:node>")
+def node_data(node):
+    types = []
 
-    if response['Count'] == 0:
-        return jsonify(message="no data for node"), 404
+    type = request.args.get('type')
+    if type is not None:
+        if type not in ['T', 'H', 'P']:
+            return jsonify(message='type must be T, H or P'), 400
+        types.append(type)
+    else:
+        types = types + ['T', 'H', 'P']
 
-    item = response['Items'][0]
-    print(item)
-    dt = item['ItemDateTime']['S']
-    try:
-        temp = float(item['Value']['N'])
-    except KeyError:
-        temp = 0
-    try:    
-        rssi = int(item['RSSI']['N'])
-    except KeyError:
-        rssi = 0
+    node_data = {}
 
-    return jsonify({'temp': temp, 'time': dt, 'rssi': rssi})
+    for type in types:
+        node_and_type = '{}_{}'.format(str(node), type)
+        response = client.query(
+            TableName='HouseData',
+            KeyConditionExpression='NodeAndType = :node_and_type',
+            ExpressionAttributeValues={
+                ':node_and_type': {
+                    'S': str(node_and_type)
+                }
+            },
+            ScanIndexForward=False,
+            Limit=1
+        )
+
+        if response['Count'] == 0:
+            continue
+
+        item = response['Items'][0]
+        dt = item['ItemDateTime']['S']
+        try:
+            value = float(item['Value']['N'])
+        except KeyError:
+            value = 0
+        try:    
+            rssi = int(item['RSSI']['N'])
+        except KeyError:
+            rssi = 0
+
+        node_data[type] = {'value': value, 'time': dt, 'rssi': rssi}
+
+    if len(node_data.keys()) == 0:
+        return jsonify(message='no data found for node'), 404
+
+    return jsonify(node_data)
