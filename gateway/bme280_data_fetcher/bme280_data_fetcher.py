030a1665c037abe134981d404351b3b12f9d01e6
Pete Andrew <peteandrew101@gmail.com>
2020-04-29 21:50:10 +0100

    Reduce frequency of bme280 readings

--- gateway/bme280_data_fetcher/bme280_data_fetcher.py
+++ gateway/bme280_data_fetcher/bme280_data_fetcher.py
@@ -1,28 +1,45 @@
-import smbus2
+import smbus
 import bme280
 import time
 import paho.mqtt.client as mqtt
 
-port = 0
+MQTT_HOST = '192.168.0.147'
+
+port = 1
 address = 0x77
-bus = smbus2.SMBus(port)
+bus = smbus.SMBus(port)
 
 calibration_params = bme280.load_calibration_params(bus, address)
+
+def publish_temperature(data):
+    client = mqtt.Client()
+    client.connect(MQTT_HOST)
+    message = '1 ' + str(data.temperature) + ' 0'
+    client.publish('temp', message)
+
+def publish_humidity(data):
+    client = mqtt.Client()
+    client.connect(MQTT_HOST)
+    message = '1 ' + str(data.humidity) + ' 0'
+    client.publish('humidity', message)
+
+def publish_pressure(data):
+    client = mqtt.Client()
+    client.connect(MQTT_HOST)
+    message = '1 ' + str(data.pressure) + ' 0'
+    client.publish('pressure', message)
+
+min_count = 0
 
 while True:
     data = bme280.sample(bus, address, calibration_params)
     print(data)
 
-    client = mqtt.Client()
-    client.connect('localhost')
-
-    message = '1 ' + str(data.temperature) + ' 0'
-    client.publish('temp', message)
-
-    message = '1 ' + str(data.pressure) + ' 0'
-    client.publish('pressure', message)
-
-    message = '1 ' + str(data.humidity) + ' 0'
-    client.publish('humidity', message)
+    if min_count % 2:
+        publish_temperature(data)
+        publish_humidity(data)
+    elif min_count % 10:
+        publish_pressure(data)
 
     time.sleep(60)
+    min_count += 1
