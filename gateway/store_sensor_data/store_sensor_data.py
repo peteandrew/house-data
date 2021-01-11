b12c819d21e42bbfa6f6e7aacbe57c48b1b451aa
Pete Andrew <peteandrew101@gmail.com>
2021-01-11 23:44:49 +0000

    Update store_sensor_data.py to subscribe to sensors/# topics

--- gateway/store_sensor_data/store_sensor_data.py
+++ gateway/store_sensor_data/store_sensor_data.py
@@ -5,34 +5,40 @@ import sys
 import paho.mqtt.client as mqtt
 
 def on_temp_message(client, userdata, msg):
+    node = msg.topic.split('/')[2]
     msg_components = str(msg.payload)[2:-1].split(' ')
     print('Temperature reading')
+    print(node)
     print(msg_components)
     try:
         c = db_conn.cursor()
-        c.execute("INSERT INTO temps VALUES (?, datetime('now'), ?, ?)", (int(msg_components[0]), int(msg_components[2]), float(msg_components[1])))
+        c.execute("INSERT INTO temps VALUES (?, datetime('now'), ?, ?)", (int(node), int(msg_components[1]), float(msg_components[0])))
         db_conn.commit()
     except sqlite3.OperationalError as e:
         print(e)
 
 def on_humidity_message(client, userdata, msg):
+    node = msg.topic.split('/')[2]
     msg_components = str(msg.payload)[2:-1].split(' ')
     print('Humidity reading')
+    print(node)
     print(msg_components)
     try:
         c = db_conn.cursor()
-        c.execute("INSERT INTO humidities VALUES (?, datetime('now'), ?, ?)", (int(msg_components[0]), int(msg_components[2]), float(msg_components[1])))
+        c.execute("INSERT INTO humidities VALUES (?, datetime('now'), ?, ?)", (int(node), int(msg_components[1]), float(msg_components[0])))
         db_conn.commit()
     except sqlite3.OperationalError as e:
         print(e)
 
 def on_pressure_message(client, userdata, msg):
+    node = msg.topic.split('/')[2]
     msg_components = str(msg.payload)[2:-1].split(' ')
     print('Pressure reading')
+    print(node)
     print(msg_components)
     try:
         c = db_conn.cursor()
-        c.execute("INSERT INTO pressures VALUES (?, datetime('now'), ?, ?)", (int(msg_components[0]), int(msg_components[2]), float(msg_components[1])))
+        c.execute("INSERT INTO pressures VALUES (?, datetime('now'), ?, ?)", (int(node), int(msg_components[1]), float(msg_components[0])))
         db_conn.commit()
     except sqlite3.OperationalError as e:
         print(e)
@@ -49,14 +55,14 @@ db_conn.commit()
 
 client = mqtt.Client()
 
-client.message_callback_add('temp', on_temp_message)
-client.message_callback_add('humidity', on_humidity_message)
-client.message_callback_add('pressure', on_pressure_message)
+client.message_callback_add('sensors/temp/+', on_temp_message)
+client.message_callback_add('sensors/humidity/+', on_humidity_message)
+client.message_callback_add('sensors/pressure/+', on_pressure_message)
 
 client.connect('localhost')
 
-client.subscribe('temp')
-client.subscribe('humidity')
-client.subscribe('pressure')
+client.subscribe('sensors/temp/+')
+client.subscribe('sensors/humidity/+')
+client.subscribe('sensors/pressure/+')
 
 client.loop_forever()
